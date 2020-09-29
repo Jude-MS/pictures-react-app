070c623e329ceb3ba7c0d588792cbe271d584e2f
Jude Monel Sanon <jsanon@tibco-support.com>
2020-09-29 18:56:38 -0500

    Updates

---
+++ precache-manifest.a29dce2456c7d721f51e20001c2ead0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d432558804219248615a0f4524ef03",
+    "url": "/jude-ms.github.io/pictures-react-app/index.html"
+  },
+  {
+    "revision": "0dc850ffff4414ca7a98",
+    "url": "/jude-ms.github.io/pictures-react-app/static/css/main.3a50d601.chunk.css"
+  },
+  {
+    "revision": "8ee0b632219f0d533928",
+    "url": "/jude-ms.github.io/pictures-react-app/static/js/2.176c98f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jude-ms.github.io/pictures-react-app/static/js/2.176c98f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc850ffff4414ca7a98",
+    "url": "/jude-ms.github.io/pictures-react-app/static/js/main.0dfd5b77.chunk.js"
+  },
+  {
+    "revision": "32006f7ad7eebb2919d6",
+    "url": "/jude-ms.github.io/pictures-react-app/static/js/runtime-main.1ddf6ad9.js"
+  }
+]);
